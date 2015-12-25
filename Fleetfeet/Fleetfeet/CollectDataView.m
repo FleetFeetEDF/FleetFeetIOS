16395a7f54fc3c44b94fe89c274bd21e791943cb
Xuehao(David) Hu <xuehaodavidhu@XuehaoDavids-MacBook-Pro.local>
2015-12-24 22:03:58 -0700

    -added NetworkConnect View -added CollectData View -added Settings View

---
+++ Fleetfeet/Fleetfeet/CollectDataView.m
@@ -0,0 +1,21 @@
+//
+//  CollectDataView.m
+//  Fleetfeet
+//
+//  Created by Xuehao(David) Hu on 12/24/15.
+//  Copyright © 2015 Xuehao(David) Hu. All rights reserved.
+//
+
+#import "CollectDataView.h"
+
+@implementation CollectDataView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Fleetfeet/Fleetfeet/SettingsView.m
@@ -0,0 +1,21 @@
+//
+//  SettingsView.m
+//  Fleetfeet
+//
+//  Created by Xuehao(David) Hu on 12/24/15.
+//  Copyright © 2015 Xuehao(David) Hu. All rights reserved.
+//
+
+#import "SettingsView.h"
+
+@implementation SettingsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
